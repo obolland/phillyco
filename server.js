fabf79b79ad9472a7e1d21b6b22e57359d06abf3
OllyB <info@ollybolland.com>
2021-03-18 15:38:22 +0000

    server updates

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.use(cors());
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
